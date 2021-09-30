c5437232d25c53eeda1ec8cfa381a04bd9f79945
Jenny Dcruz <jendcruz23@gmail.com>
2021-09-30 09:48:48 -0400

    added: empty J1 and J2 Controllers

---
+++ ASP.NET_Assignment2/Controllers/J1ProblemController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ASP.NET_Assignment2.Controllers
+{
+    public class J1ProblemController : ApiController
+    {
+    }
+}
