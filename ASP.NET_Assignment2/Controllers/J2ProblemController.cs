abc2a9079bfe1414765966899e8fbcc28f69e9b3
Jenny Dcruz <jendcruz23@gmail.com>
2021-09-30 11:10:24 -0400

    added: J2 problem solution ( sleepy emoji pending)

--- ASP.NET_Assignment2/Controllers/J2ProblemController.cs
+++ ASP.NET_Assignment2/Controllers/J2ProblemController.cs
@@ -7,8 +7,74 @@ using System.Web.Http;
 
 namespace ASP.NET_Assignment2.Controllers
 {
-    //J2 Problem from 2007
+    //J2 Problem from 2007: I Speak TXTMSG
     public class J2ProblemController : ApiController
     {
+        //GET: api/j2problem/txtmsg/shortform
+        [HttpGet]
+        [Route("api/j2problem/txtmsg/{shortform}")]
+
+        public string TXTMSG(string ShortForm)
+       {
+            string outputMessage;
+
+            switch (ShortForm)
+            {
+
+                case "CU":
+                    outputMessage = "see you";
+                    break;
+
+                case ":-)":
+                    outputMessage = "I'm happy";
+                    break;
+
+                case ":-(":
+                    outputMessage = "I'm unhappy";
+                    break;
+
+                case ";-)":
+                    outputMessage = "wink";
+                    break;
+
+                case ":-P":
+                    outputMessage = "stick out my tongue";
+                    break;
+
+                case "(~.~)":
+                    outputMessage = "sleepy";
+                    break;
+
+                case "TA":
+                    outputMessage = "totally awesome";
+                    break;
+
+                case "CCC":
+                    outputMessage = "Canadian Computing Competition";
+                    break;
+
+                case "CUZ":
+                    outputMessage = "because";
+                    break;
+
+                case "TY":
+                    outputMessage = "thank-you";
+                    break;
+
+                case "YW":
+                    outputMessage = "you're welcome";
+                    break;
+
+                case "TTYL":
+                    outputMessage = "talk to you later";
+                    break;
+
+                default:
+                    outputMessage = "Invalid Input";
+                    break;
+            }
+
+            return outputMessage;
+        }
     }
 }
