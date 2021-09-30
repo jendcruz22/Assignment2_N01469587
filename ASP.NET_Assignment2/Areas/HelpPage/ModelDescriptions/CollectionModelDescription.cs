079b8a1bf7189e08e2ad36264de2bbb3b69526cf
Jenny Dcruz <jendcruz23@gmail.com>
2021-09-30 09:45:36 -0400

    Add project files.

---
+++ ASP.NET_Assignment2/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ASP.NET_Assignment2.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ASP.NET_Assignment2/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ASP.NET_Assignment2.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
